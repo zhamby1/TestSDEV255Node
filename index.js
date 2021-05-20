015143cc8420deea2c1993bfd314b0d160244bc4
zhamby1 <zhamby1@ivytech.edu>
2021-05-19 21:57:35 -0400

    new text added

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello Express app!')
+  res.send('Hello Express app how are you!')
 });
 
 app.listen(3000, () => {
